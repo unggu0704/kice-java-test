d694984bad58a5f69fae38d898a9f1cf28e6179b
UNNGU0704 <kyuhung1001@gmail.com>
2025-09-02 22:27:14 +0900

    문제2 풀이

--- SP_TEST2/src/SP_TEST.java
+++ SP_TEST2/src/SP_TEST.java
@@ -1,10 +1,17 @@
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.*;
 import java.util.concurrent.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class SP_TEST {
+    static final String LOG_PATH = "./SP_TEST2/LOG_DATA.TXT";
+    private static final String LOG_PATTERN =
+            "\\[\\d{4}-\\d{2}-\\d{2} (\\d{2}):\\d{2}:\\d{2}\\] ([A-Z]+) (\\w+).*";
     
     static class WorkerPolicy {
         String id;
@@ -19,7 +26,7 @@ public class SP_TEST {
     static class WorkerResult {
         String worker_id;
         int processed_lines;
-        Map<String, Integer> log_levels;
+        Map<String, Integer> log_levels = new HashMap<>();
         long processing_time_ms;
     }
     
@@ -28,7 +35,7 @@ public class SP_TEST {
         
         while (true) {
             String logFilePath = scanner.nextLine();
-            processWithWorkers(logFilePath);
+            processWithWorkers("./SP_TEST2/WorkerPolicy.json");
         }
     }
     
@@ -38,7 +45,78 @@ public class SP_TEST {
         // TODO: 3. CompletableFuture로 멀티스레드 처리
         // TODO: 4. 각 워커별 결과를 RESULT_<WorkerID>.json에 저장
         // TODO: 5. 통합 결과 콘솔 출력
-        
+        StringBuilder sb;
+        try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
+            sb = new StringBuilder();
+            String line;
+            while ((line = br.readLine()) != null) {      // 한 줄씩 읽음
+               sb.append(line);
+            }
+        }
 
+        Gson gson = new Gson();
+        List<WorkerPolicy> workers = gson.fromJson(sb.toString(), WorkerPolicyConfig.class).workers;
+
+        List<String> lines = Files.readAllLines(Paths.get(LOG_PATH));
+        int lineCount = lines.size(); // 로그 파일 전체 카운트
+        int pivot = 0;
+
+        //비동기 작업 시작
+        List<CompletableFuture> futures = new ArrayList<>();
+        for (WorkerPolicy workerPolicy : workers) {
+            // 읽어야할 페이지를 계산
+            int chunks = (int) (lineCount * (workerPolicy.processing_ratio / 100.0));
+            int endpoint = Math.min(lineCount, pivot + chunks);
+
+            List<String> processingLine = new ArrayList<>();
+            for (;pivot < endpoint; pivot++) {
+                processingLine.add(lines.get(pivot));
+            }
+
+            CompletableFuture<Void> future = CompletableFuture.supplyAsync(() -> function(workerPolicy.id, processingLine))
+                            .thenAccept( workerResult -> {
+                                FileWriter fw = null;
+                                try {
+                                    fw = new FileWriter("./SP_TEST2/" + workerPolicy.id + ".json");
+                                    gson.toJson(workerResult, fw);
+                                    fw.flush();
+                                    fw.close();
+
+                                } catch (IOException e) {
+                                    throw new RuntimeException(e);
+                                }
+                            });
+            futures.add(future);
+        }
+
+        CompletableFuture.allOf(futures.toArray(new CompletableFuture[0]));
+    }
+
+    private static WorkerResult function(String workerid, List<String> processingLine) {
+        long beforeTime = System.currentTimeMillis();
+        Pattern pattern = Pattern.compile(LOG_PATTERN);
+        WorkerResult workerResult = new WorkerResult();
+        Map<String,Integer> logLevelMap = new HashMap<>();
+
+        for (String line : processingLine) {
+            Matcher matcher = pattern.matcher(line);
+            matcher.matches();
+
+            String logLevel = matcher.group(2);
+            logLevelMap.put(logLevel, logLevelMap.getOrDefault(logLevel, 0) + 1);
+        }
+
+        List<String> logLevelList = new ArrayList<>(logLevelMap.keySet());
+        logLevelList.sort( (i1, i2) -> logLevelMap.get(i2) -logLevelMap.get(i1) );
+
+        for (String key : logLevelList)
+            workerResult.log_levels.put(key, logLevelMap.get(key));
+
+        workerResult.worker_id = workerid;
+        workerResult.processed_lines = processingLine.size();
+        long afterTime = System.currentTimeMillis();
+        workerResult.processing_time_ms = (afterTime - beforeTime)/10;
+
+        return workerResult;
     }
 }
